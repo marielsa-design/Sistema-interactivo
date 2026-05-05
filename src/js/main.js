5ca57fdf216af3ba8995627aee422f19bb50387d
Marielsa <marielsa.milagro@gmail.com>
2026-05-05 16:05:32 -0500

    Sistema-interactivo

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+const mensaje ="hola mundo";
+
+console.log(`console.log= ${mensaje}`);
+console.log(`%cconsole.log con estilos= ${mensaje}`, "background-color: #D9CEFF; border-radius: 50px; font-weight: bold; padding-top: 5px; padding-right: 8px; padding-bottom: 5px; padding-left: 8px;");
