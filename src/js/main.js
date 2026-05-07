4b6a099adf503ade7cb0522879a671a92249b5a5
Marielsa <marielsa.milagro@gmail.com>
2026-05-07 16:08:41 -0500

    Sistema-interactivo

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
-const mensaje ="hola mundo";
 
-console.log(`console.log= ${mensaje}`);
-console.log(`%cconsole.log con estilos= ${mensaje}`, "background-color: #D9CEFF; border-radius: 50px; font-weight: bold; padding-top: 5px; padding-right: 8px; padding-bottom: 5px; padding-left: 8px;");
+const frutas = ["Banana", "Orange", "Apple", "Mango"];
+
+for(let i = 0; i < frutas.length; i++){
+    console.log(frutas[i])
+}
