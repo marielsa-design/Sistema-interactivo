97faabef7df3b99cc5ba2cb1d700a6107846ae74
Marielsa <marielsa.milagro@gmail.com>
2026-05-08 15:58:08 -0500

    Sistema-interactivo

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,33 @@
+// Solicitar el nombre
 
-const frutas = ["Banana", "Orange", "Apple", "Mango"];
+let userName = prompt("Ingresa tu nombre:");
 
-for(let i = 0; i < frutas.length; i++){
-    console.log(frutas[i])
+if (userName === "" || userName === null) {
+    console.error("Error: nombre inválido");
+    alert("Debes ingresar tu nombre");
+} else {
+    console.log("Hola", userName);
+}
+
+// Solicitar el la edad
+
+let userAge = prompt("Ingresa tu edad:");
+
+if (userAge === "" || userAge === null) {
+    console.error("Error: edad inválido");
+    alert("Debes ingresar tu edad");
+} else {
+    console.log("Tu edad: ", userAge);
+}
+
+// Verificar "es menor o mayor de edad"
+
+if (userAge < 18) { // Comprueba si es mayor o menor de edad de un usuario de nombre
+
+    console.log("Hola" + userName + ", eres menor de edad")
+    alert("Hola " + userName + ", eres menor de edad")
+} else {
+
+    console.log("Hola" + userName + ", eres mayor de edad")
+    alert("Hola " + userName + ", eres mayor de edad")
 }
