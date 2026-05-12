44e7f704938e408e47c40575d0e05292ef57a24c
Marielsa <marielsa.milagro@gmail.com>
2026-05-12 16:03:11 -0500

    Sistema-interactivo

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,42 @@
-// Creación de un sistema interactivo de mensajes
+// // Creación de un sistema interactivo de mensajes
 
-// Solicitar el nombre
+// // Solicitar el nombre
 
-let userName = prompt("Ingresa tu nombre:");
+// let userName = prompt("Ingresa tu nombre:");
 
-if (userName === "" || userName === null) {
-    console.error("Error: nombre inválido");
-    alert("Debes ingresar tu nombre");
-} else {
-    console.log("Hola", userName);
+// if (userName === "" || userName === null) {
+//     console.error("Error: nombre inválido");
+//     alert("Debes ingresar tu nombre");
+// else {
+//         console.log("Hola ", userName);
+//     }
+// }
+
+// // Solicitar el la edad
+
+// let userAge = prompt("Ingresa tu edad:");
+
+// if (userAge === "" || userAge === null) {
+//     console.error("Error: edad inválido");
+//     alert("Debes ingresar tu edad");
+// } else {
+//     console.log("Tu edad: ", userAge);
+// }
+
+// // Resultado: Verificar "Si es menor o mayor de edad"
+
+// if (userAge < 18) { // Comprueba si es mayor o menor de edad de un usuario de nombre
+
+//     console.log("Hola" + userName + ", eres menor de edad")
+//     alert("Hola " + userName + ", eres menor de edad")
+// } else {
+
+//     console.log("Hola" + userName + ", eres mayor de edad")
+//     alert("Hola ", + userName + ", eres mayor de edad")
+// }
+
+const frutas = ["manzana", "naranja", "cereza", "pera"]
+
+for(let i = 0; i < frutas.length; i++){
+    console.log(frutas[i])
 }
-
-// Solicitar el la edad
-
-let userAge = prompt("Ingresa tu edad:");
-
-if (userAge === "" || userAge === null) {
-    console.error("Error: edad inválido");
-    alert("Debes ingresar tu edad");
-} else {
-    console.log("Tu edad: ", userAge);
-}
-
-// Resultado: Verificar "Si es menor o mayor de edad"
-
-if (userAge < 18) { // Comprueba si es mayor o menor de edad de un usuario de nombre
-
-    console.log("Hola" + userName + ", eres menor de edad")
-    alert("Hola " + userName + ", eres menor de edad")
-} else {
-
-    console.log("Hola" + userName + ", eres mayor de edad")
-    alert("Hola ", + userName + ", eres mayor de edad")
-}
-
