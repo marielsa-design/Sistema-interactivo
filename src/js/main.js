94c46efecfc7b31c8d5bd438b0e2bbb37a5f7f54
Marielsa <marielsa.milagro@gmail.com>
2026-05-14 16:10:58 -0500

    Sistema-interactivo

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,35 @@
-// // Creación de un sistema interactivo de mensajes
+// Creación de un sistema interactivo de mensajes
 
-// // Solicitar el nombre
+// Solicitar el nombre
+let userName = prompt("Ingresa tu nombre:");
 
-// let userName = prompt("Ingresa tu nombre:");
+if (userName === "" || userName === null) {
+    console.error("Error: nombre inválido");
+    alert("Debes ingresar tu nombre");
+} else {
 
-// if (userName === "" || userName === null) {
-//     console.error("Error: nombre inválido");
-//     alert("Debes ingresar tu nombre");
-// else {
-//         console.log("Hola ", userName);
-//     }
-// }
+    // Solicitar la edad
+    let userAge = Number(prompt("Ingresa tu edad:"));
 
-// // Solicitar el la edad
+    if (userAge === 0 || isNaN(userAge)) {
 
-// let userAge = prompt("Ingresa tu edad:");
+        console.error("Error: edad inválida");
+        alert("Debes ingresar una edad válida");
 
-// if (userAge === "" || userAge === null) {
-//     console.error("Error: edad inválido");
-//     alert("Debes ingresar tu edad");
-// } else {
-//     console.log("Tu edad: ", userAge);
-// }
+    } else {
 
-// // Resultado: Verificar "Si es menor o mayor de edad"
+        console.log("Tu edad:", userAge);
 
-// if (userAge < 18) { // Comprueba si es mayor o menor de edad de un usuario de nombre
+        // Verificar si es menor o mayor de edad
+        if (userAge < 18) {
 
-//     console.log("Hola" + userName + ", eres menor de edad")
-//     alert("Hola " + userName + ", eres menor de edad")
-// } else {
+            console.log("Hola " + userName + ", eres menor de edad");
+            alert("Hola " + userName + ", eres menor de edad");
 
-//     console.log("Hola" + userName + ", eres mayor de edad")
-//     alert("Hola ", + userName + ", eres mayor de edad")
-// }
+        } else {
 
+            console.log("Hola " + userName + ", eres mayor de edad");
+            alert("Hola " + userName + ", eres mayor de edad");
+        }
+    }
+}
