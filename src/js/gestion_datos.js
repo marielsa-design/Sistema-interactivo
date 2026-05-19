f66a629c7d99da65e3a9ff6d71cdad72f57f46c1
Marielsa <marielsa.milagro@gmail.com>
2026-05-19 16:27:41 -0500

    feat: actualizar

---
+++ src/js/gestion_datos.js
@@ -0,0 +1,15 @@
+// Cada producto debe contener las propiedades:
+// id (clave única del producto)
+// nombre (nombre del producto)
+// precio (valor numérico)
+
+
+// listas de los Productos
+
+let producto = {
+        id: 1,
+        nombre: "Laptop",
+        pecio: 25000,
+    };
+
+console.log(producto);
